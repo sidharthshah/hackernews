c7639fbd9089220f8c25ea1ff9ce3e9b2ffbb812
Sidharth Shah <sidharth@fafadiatech.com>
2013-06-07 09:19:27 +0530

    added title field to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 require 'thumbs_up'
 class Post < ActiveRecord::Base
   acts_as_voteable
-  attr_accessible :link
+  attr_accessible :link, :title
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130607032909) do
+ActiveRecord::Schema.define(:version => 20130607034555) do
 
   create_table "posts", :force => true do |t|
     t.string   "link"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "title"
   end
 
   create_table "users", :force => true do |t|
