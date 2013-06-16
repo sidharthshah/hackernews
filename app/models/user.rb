42948c644bb3fc5858be6d66365a4f62b740d249
Sidharth Shah <sidharth@fafadiatech.com>
2013-06-16 11:22:29 +0530

    refactored code to allow username for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  validates_presence_of :username
+  validates_uniqueness_of :username
   has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :login
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :posts
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me, :posts, :login
   # attr_accessible :title, :body
   acts_as_voter
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130616052705) do
+ActiveRecord::Schema.define(:version => 20130616053614) do
 
   create_table "posts", :force => true do |t|
     t.string   "link"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20130616052705) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
