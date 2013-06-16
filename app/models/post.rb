d0ab1723bd03859ec9c0007f658497c324091c17
Sidharth Shah <sidharth@fafadiatech.com>
2013-06-16 11:05:32 +0530

    added user_id to Post and defined relation with User

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 require 'thumbs_up'
 class Post < ActiveRecord::Base
+  belongs_to :user
   acts_as_voteable
-  attr_accessible :link, :title
+  attr_accessible :link, :title, :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130613083625) do
+ActiveRecord::Schema.define(:version => 20130616052705) do
 
   create_table "posts", :force => true do |t|
     t.string   "link"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "title"
+    t.integer  "user_id"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
