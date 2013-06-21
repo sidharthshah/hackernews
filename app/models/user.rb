5974824faeda6d966fa31653c343590545598695
Prasen <prasen@fafadiatech.com>
2013-06-21 16:38:44 +0530

    Devise Issue resolved.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,18 @@ class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me, :posts, :login
   # attr_accessible :title, :body
   acts_as_voter
+
+### This is the correct method you override with the code above
+### def self.find_for_database_authentication(warden_conditions)
+### end 
+  def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+  end
+
+
 end
